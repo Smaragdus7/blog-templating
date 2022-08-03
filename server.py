511692f7cc39299ffa1b8f0e730a7b4569e69f2b
Claudia Ibarra <Smaragdus7@users.noreply.github.com>
2022-08-03 14:10:52 -0500

    contact working

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
-from post import Post
+from flask import Flask, render_template, request
+import smtplib
 import requests
 
 data = requests.get("https://api.npoint.io/2fba7f6009a9cfff9ae9").json()
+OWN_EMAIL = ""
+OWN_PASSWORD = ""
 
 app = Flask(__name__)
 
@@ -17,11 +19,6 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
@@ -29,3 +26,20 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data_f = request.form
+        data_f = request.form
+        send_email(data_f["name"], data_f["email"], data_f["phone"], data_f["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
